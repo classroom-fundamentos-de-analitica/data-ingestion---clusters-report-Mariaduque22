4ab12ae2001d52508c7f1fbae2f5839ae964bc6a
Maria Paula Duque <92931247+Mariaduque22@users.noreply.github.com>
2022-11-06 15:28:42 -0500

    taller 4

--- pregunta.py
+++ pregunta.py
@@ -10,12 +10,38 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
-
+import re
 
 def ingest_data():
 
-    #
-    # Inserte su código aquí
-    #
+    with open('clusters_report.txt') as report:
+        fila = report.readlines()
 
+    clusters = []
+    cluster = [0, 0, 0, '']
+    fila = fila[4:]
+
+
+    for i in fila:
+        if re.match('^ +[0-9]+ +', i):
+            numeroCluster, cantidadPalabras, porcentajePalabras, *principalesPalabras = i.split()
+            cluster[0] = int(numeroCluster)
+            cluster[1] = int(cantidadPalabras)
+            cluster[2] = float(porcentajePalabras.replace(',','.')) 
+            principalesPalabras.pop(0) 
+            principalesPalabras = ' '.join(principalesPalabras)
+            cluster[3] = cluster[3] + principalesPalabras
+
+    
+        elif re.match('^ +[a-z]', i):
+            principalesPalabras = i.split()
+            principalesPalabras = ' '.join(principalesPalabras)
+            cluster[3] = cluster[3] + ' ' + principalesPalabras
+
+        elif re.match('^\n', i) or re.match('^ +$', i):
+            cluster[3] = cluster[3].replace('.', '')
+            clusters.append(cluster)
+            cluster = [0, 0, 0, '']
+
+    df = pd.DataFrame (clusters, columns = ['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave'])
     return df
